c090ce7512abd7c81ce57cc20dbb5df4222fd6c5
Robert Muresan <robotzelu89@gmail.com>
2025-01-16 17:48:38 +0000

    Solved lab

--- src/challenges.js
+++ src/challenges.js
@@ -13,13 +13,34 @@ const repeatedWords = [
   "matter"
 ];
 
-function howManyTimes() {}
+function howManyTimes(wordsArray, wordToSearch) {
+
+    let count = 0;
+    for (let word of wordsArray) {
+      if (word === wordToSearch) {
+        count++;
+      }
+    }
+    return count;
+  }
+
 
 
 
 
 // Iteration 2 | Number Sequence
-function createSequence() {}
+function createSequence(number) {
+
+  if (number === 0) {
+    return [];
+  }
+
+  let arr = [];
+  for (let i = 0; i <= number; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
 
 
@@ -27,7 +48,14 @@ function createSequence() {}
 // Iteration 3 | Multiply for Each
 const numbers = [1, 2, 5, 10, 13, 50];
 
-function multiplyBy() {}
+function multiplyBy(arr,multiplier) {
+  let newArr = [];
+ arr.forEach((num) => {
+    newArr.push(num * multiplier);
+  });
+  return newArr;
+} 
+ 
 
 
 
@@ -36,7 +64,12 @@ function multiplyBy() {}
 const original = ["cat", "dog", "fish", "bird", "cat", "fish"];
 const toRemove = ["cat", "dog"];
 
-function filterOut() {}
+function filterOut(original, toRemove) {
+  if (original.length === 0) {
+    return null;
+  }
+  return original.filter((element) => !toRemove.includes(element));
+}
 
 
 
@@ -56,7 +89,13 @@ const duplicateWords = [
   "bring"
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(duplicateWords) {
+  if (duplicateWords.length === 0) {
+    return null;
+  }
+  return duplicateWords.filter((element, index) => duplicateWords.indexOf(element) === index);
+}
+
 
 
 
@@ -85,4 +124,23 @@ const matrix = [
   [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ];
 
-function greatestProduct() {}
+function greatestProduct(matrix) {
+  let greatestProduct = 0;
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (j < matrix[i].length - 3) {
+          let product = matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
+          if (product > greatestProduct) {
+            greatestProduct = product;
+          }
+        }
+        if (i < matrix.length - 3) {
+          let product = matrix[i][j] * matrix[i + 1][j] * matrix[i + 2][j] * matrix[i + 3][j];
+          if (product > greatestProduct) {
+            greatestProduct = product;
+          }
+        }
+      }
+    }
+    return greatestProduct;
+}
